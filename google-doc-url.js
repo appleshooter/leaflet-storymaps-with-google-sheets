2978b520373d91b2c9c8052e2a30b550cfe9cbbc
appleshooter <54536389+appleshooter@users.noreply.github.com>
2019-08-27 10:37:04 +0530

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1lYeO6aGKmldGQKKIcYLDIXqy4LGoxLEIY6rn4xMdsS8/edit#gid=0';
